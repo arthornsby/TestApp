cf632e3262d1990faa4f6838bc7e84bf6ddb6dbf
Art Hornsby <arthornsby@gmail.com>
2012-12-14 16:14:08 -0500

    update static pages stylesheets etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 TestApp::Application.routes.draw do
+
+  root :to => "static_pages#home"
+
   get "static_pages/home"
-
   get "static_pages/help"
-
   get "static_pages/about"
+  get "static_pages/contact"
 
   resources :microposts
 
